16f6dcb5852801cec3e73e3d86d4bfb4697d103b
kelly mutch <kellymutch@gmail.com>
2016-06-14 14:55:03 -0700

    basic scafold3

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 'use strict';
 var userName = prompt('Greetings!! What is your name');
 alert ('Welcome ' + userName + ' glad you are here!');
-
-var answer1 = prompt('Does Sam have three cats?')
-if (answer1 === yes) {
-tell them
+var answer1 = prompt('Does Sam have three cats?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert ('Right');
 
 }
-  else {
-    wrong
-  }
+else {
+  alert ('wrong');
+}
