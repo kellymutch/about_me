f673d4258fd311301c18f1465724b5fb4f513ee6
kelly mutch <kellymutch@gmail.com>
2016-06-14 14:35:14 -0700

    basic scafold2

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 'use strict';
-alert(Yaaaaa);
+var userName = prompt('Greetings!! What is your name');
+alert ('Welcome ' + userName + ' glad you are here!');
+
+var answer1 = prompt('Does Sam have three cats?')
+if (answer1 === yes) {
+tell them
+
+}
+  else {
+    wrong
+  }
