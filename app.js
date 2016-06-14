ca97e78b2a80c4b36aeedb0b01ee188b327b8d3f
kelly mutch <kellymutch@gmail.com>
2016-06-14 16:43:47 -0700

    basic changes beginningon code

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ else {
 }
 console.log (answer1);
 
-var answer2 = prompt('?').toLowerCase();
+var answer2 = prompt('Has Kelly ever lived else where?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert ('Yes, I lived in Santa Fe, New Mexico for 15v years.');
+}
+else {
+  alert ('That is incorrect');
+}
+console.log (answer2);
+
+
+
 var answer3 = prompt('?').toLowerCase();
 var answer4 = prompt('?').toLowerCase();
 var answer5 = prompt('?').toLowerCase();
