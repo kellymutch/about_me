b0c5ce7b89ca6ae97b9b0f4e74d9dc5aa6bfc80e
kelly mutch <kellymutch@gmail.com>
2016-06-14 19:43:06 -0700

    Why did I not set up git on the desktop still

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 var userName = prompt('Greetings!! What is your name');
-alert ('Welcome ' + userName + '. We are going to play a guessing game');
+alert ('Welcome ' + userName + '. We are going to play a guessing game.');
 console.log ('Greeting answered');
 var answer1 = prompt ('Is Kelly a native Seattleite?').toLowerCase();
 if (answer1 === 'yes' || answer1 === 'y') {
@@ -18,12 +18,31 @@ if (answer2 === 'yes' || answer2 === 'y') {
 else {
   alert ('That is incorrect');
 }
-console.log (answer2);
+console.log (answer2 + (' answer to 2'));
 
-var answer3 = prompt('Has Kelly done non IT work').toLowerCase();
+var answer3 = prompt('Has Kelly done non IT work.').toLowerCase();
+if (answer3 === 'yes' || answer3 === 'y') {
+  alert ('Yes, I have been a non profit director, a ditch digger, janitor, farm and stable manager and a few other things.');
+}
+else {
+  alert ('That is incorrect');
+}
+console.log (answer3 + (' answer to 3'));
 
+var answer4 = prompt('Does Kelly ever go outside?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert ('Yes, I golf, bike, hike and love to do new things. My last adventure was salmon/halibut fishing in the gulf of Alaska.');
+}
+else {
+  alert ('That is incorrect');
+}
+console.log (answer4 + (' answer to 4'));
 
-
-
-var answer4 = prompt('4').toLowerCase();
-var answer5 = prompt('5').toLowerCase();
+var answer5 = prompt('Is Kelly creative and fun and whimsical ').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert ('Not really but after a beer or two, I start to find myself funny.');
+}
+else {
+  alert ('Not really but after a beer or two, I start to find myself funny.');
+}
+console.log (answer5 + (' answer to 5'));
